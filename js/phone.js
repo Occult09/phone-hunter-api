34defd063e874f4a21a8decdfa0da65438275a04
Occult <maheermusfiq221@gmail.com>
2025-05-06 23:12:04 +0600

    added loading spinner

--- js/phone.js
+++ js/phone.js
@@ -47,11 +47,26 @@ const displayPhones = phones => {
         // 4. append child
         phoneContainer.appendChild(phoneCard)
     });
+
+    // hide loading spinner
+    toggleLoadingSpinner(false)
 }
 
 // handle search button
 const handleSearch = () => {
+    toggleLoadingSpinner(true)
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value
     loadPhone(searchText)
+}
+
+// loading spinner
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner')
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+    }
+    else {
+        loadingSpinner.classList.add('hidden')
+    }
 }
